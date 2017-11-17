d11f204e928b75bc6f7f75b802d8501c922aa3c7
David Jerome <GlassToeStudio@gmail.com>
2017-11-17 08:31:04 -0600

    Remove ignored files

--- ConwaysGameofLife/cgol/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" Entry point to conways_game_of_life. """
-import utils
-import make_gif
-import make_image
-import conways_game_of_life
-
-if __name__ == '__main__':
-    conways_game_of_life.main()
